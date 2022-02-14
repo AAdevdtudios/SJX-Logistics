b5997b7e724ad4dd69db0c337766086ed8cda74e
AAdevStudios <86613826+AAdevdtudios@users.noreply.github.com>
2022-02-14 15:30:47 +0100

    MVC removed

---
+++ Migrations/20220214140534_dbs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SjxLogistics.Migrations
+{
+    public partial class dbs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
