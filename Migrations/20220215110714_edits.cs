0f6e19a12647e1665c6658323e7759cf6c94e9e7
AAdevStudios <86613826+AAdevdtudios@users.noreply.github.com>
2022-03-23 10:33:25 +0100

    Untold

---
+++ Migrations/20220215110714_edits.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SjxLogistics.Migrations
+{
+    public partial class edits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RecieverPhone",
+                table: "Order",
+                newName: "ReceiversPhone");
+
+            migrationBuilder.RenameColumn(
+                name: "CustormerEmail",
+                table: "Order",
+                newName: "CustomersEmail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReceiversPhone",
+                table: "Order",
+                newName: "RecieverPhone");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomersEmail",
+                table: "Order",
+                newName: "CustormerEmail");
+        }
+    }
+}
